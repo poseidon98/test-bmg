466649cb7a0282c2fc51a1307b9fcdaf609259bc
Krinker98 <76026062+poseidon98@users.noreply.github.com>
2022-03-24 09:48:11 +0700

    deret angka

--- forangka.php
+++ forangka.php
@@ -1,5 +1,11 @@
 <?php
-
 for ($n = 1; $n < 51; $n++) {
-    echo $n . ", ";
+    if ($n % 3 == 0) {
+        echo "Frontend, ";
+    } else if ($n % 5 == 0) {
+        echo "Backend, ";
+    } else {
+        echo $n . ", ";
+    }
 }
+
